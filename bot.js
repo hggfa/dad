94af717feae338fd0acd346707a2bf907f158bb6
hggfa <45233355+hggfa@users.noreply.github.com>
2018-12-01 19:46:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -809,6 +809,43 @@ client.on("message", message => {
   });
   
   
+
+
+client.on('message', message => {
+if (message.content.split(' ')[0] == '=bc')
+ message.guild.members.forEach( member => {
+         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+		 if(!message.channel.guild) return message.reply('** This command only for servers **');
+member.send( `${member} ! ` + "**" + message.guild.name + " : ** " + message.content.substr(3));
+                                                            message.delete();
+});
+});
+client.on("message", message => {
+    var prefix = "=";
+ 
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+						 if(!message.channel.guild) return message.reply('** This command only for servers **');
+ if (!args[1]) {
+                                let embed3 = new Discord.RichEmbed()
+                                .setDescription(":white_check_mark: | تم ارسال للكل رساله فارغه")
+                                .setColor("RANDOM")
+                                message.channel.sendEmbed(embed3);
+                            } else {
+
+                            let embed4 = new Discord.RichEmbed()
+                                                            .setDescription(':white_check_mark: | تم ارسال للكل الرساله !')
+                                                            .setColor("#008000")
+                                message.channel.sendEmbed(embed4);
+                                                      message.delete();
+                            }
+                          }
+});
+
+
+
+
   
 client.on('message', function(message) {
     if(message.content.startsWith(prefix + 'roll')) {
